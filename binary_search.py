c9d6c32bda82cf957301f059d896d859d90c1d63
a1xc0m <ialexx.ak@gmail.com>
2021-12-04 17:17:40 +0300

    first comit

---
+++ binary_search.py
@@ -0,0 +1,35 @@
+a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+def bubbleSort(a):
+    l = len(a)
+    for i in range(l - 1):
+        for j in range(l - i - 1):
+            if a[j] > a[j + 1]:
+                temp = a[j]
+                a[j] = a[j + 1]
+                a[j + 1] = temp
+
+def binarySearch(a):
+    value = int(input())
+    
+    mid = len(a) // 2
+    low = 0
+    high = len(a) - 1
+    
+    while a[mid] != value and low <= high:
+        if value > a[mid]:
+            low = mid - 1
+        else:
+            high = mid - 1
+        mid = (low + high) // 2
+        
+    if low > high:
+        print('No value')
+    else:
+        print("ID = ", mid)
+        
+
+bubbleSort(a)
+print(a)
+binarySearch(a)
+print(a)

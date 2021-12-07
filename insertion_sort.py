bccc2fca1170fd78a5bac0533a579de4532a4f27
a1xc0m <ialexx.ak@gmail.com>
2021-12-08 00:07:54 +0300

    check

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,14 +5,15 @@ a = [randint(0, 100) for i in range(n)]
 print('Getting array: ', a)
 
 def insertionSort(a):
-    for i in range(len(a) - 1):
-        j = i - 1
-        key = a[i]
-        while a[j] > key and j >= 0:
-            a[j + 1] = a[j]
-            j = j - 1
-        a[j + 1] = key
-    return
+    l = len(a)
+    for i in range(1, l):
+        for j in range(i, 0, -1):
+            if a[j] < a[j - 1]:
+                temp = a[j]
+                a[j] = a[j - 1]
+                a[j - 1] = temp
+            else:
+                break
             
 insertionSort(a)
 print('Result: ', a)
